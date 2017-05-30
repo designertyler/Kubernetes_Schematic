1ec28c093c2435cb98bf2e44c886fcefeaf7a12f
Tyler <trtate@gmail.com>
2017-05-30 15:41:04 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 data "ibmcloud_cs_cluster" "cluster_foo" {
-    cluster_name_id = "FOO"
+    cluster_name_id = "${var.cluster_name_id}"
     org_guid = "${var.org_guid}"
     space_guid = "${var.space_guid}"
     account_guid = "${var.account_guid}"
