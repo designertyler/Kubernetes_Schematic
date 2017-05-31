9a609ab8464ee8f8093320d5e6da51789b0508e2
Tyler <trtate@gmail.com>
2017-05-31 10:07:06 -0500

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -4,5 +4,5 @@ variable "cluster_name_id" {
 variable "org_guid" {}
 variable "space_guid" {}
 variable "account_guid" {}
-variable "ibmcloud_sl_username" {}
-variable "ibmcloud_sl_api_key" {}
+variable "sl_username" {}
+variable "sl_api_key" {}
