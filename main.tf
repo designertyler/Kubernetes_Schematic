570dbe7dc36a9337b1f70e582c68e26783de3793
Tyler <trtate@gmail.com>
2017-05-31 15:40:33 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,6 @@ resource "ibmcloud_cs_cluster" "foobernetes2" {
     datacenter = "dal10"
     machine_type = "free"
     isolation = "public"
-    public_vlan_id = "vlan"
-    private_vlan_id = "vlan"
-    subnet_id = ["1154643"]
     workers = [{name="worker1",  action="add"},]
     org_guid = "${var.org_guid}"
     space_guid = "${var.space_guid}"
