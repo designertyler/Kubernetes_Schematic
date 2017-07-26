0a2f0c95f0f4ef1868091ab0252169f7cf1598dd
Tyler <trtate@gmail.com>
2017-07-26 13:57:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,8 +7,4 @@ resource "ibm_container_cluster" "foobernetes" {
     datacenter = "dal10"
     machine_type = "free"
     isolation = "public"
-    workers = [{name="worker1",  action="add"},]
-    org_guid = "${var.org_guid}"
-    space_guid = "${var.space_guid}"
-    account_guid = "${var.account_guid}"
 }
